8441f3c394326a8afc08598127b7869fdb19db45
Jordan Ackerman <jork41989@gmail.com>
2020-03-27 20:57:57 -0400

    added validations for user

---
+++ supplydrop-api/routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
+
+
+
+
+
+module.exports = router;
