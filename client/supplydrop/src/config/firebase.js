4c519e46c7204c71c1a0b94f79802495170c7248
Jordan Ackerman <jork41989@gmail.com>
2020-03-28 14:16:41 -0400

    added login and register requests

--- client/supplydrop/src/config/firebase.js
+++ client/supplydrop/src/config/firebase.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import firebase from 'firebase';
-const firebaseConfig = {
-    apiKey: "AIzaSyCY_qBRcje4QmwAQCe_SWaGgoDznoCvsZA",
-    authDomain: "supplydrop-e7947.firebaseapp.com",
-    databaseURL: "https://supplydrop-e7947.firebaseio.com",
-    projectId: "supplydrop-e7947",
-    storageBucket: "supplydrop-e7947.appspot.com",
-    messagingSenderId: "692804449781",
-    appId: "1:692804449781:web:69785e66fbf8e318f01cbc",
-    measurementId: "G-2TCXVNEM01"
-};
-
-const fire = firebase.initializeApp(firebaseConfig);
-export default fire;
-=======
 import * as firebase from 'firebase/app';
 import "firebase/auth";
 console.log(process)
@@ -29,4 +13,3 @@ const app = firebase.initializeApp({
         })
 
 export default app;
->>>>>>> master
