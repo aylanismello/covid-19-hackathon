48a1917a0cb95a4c749cca490c30ae1a7f27e746
Marvin <yma010@ucr.edu>
2020-03-27 20:00:24 -0700

    filled out SignUp Component

--- client/supplydrop/src/components/auth/SignUp.jsx
+++ client/supplydrop/src/components/auth/SignUp.jsx
@@ -1,5 +1,42 @@
-import React, {useState} from 'react';
+import React, { useCallback } from 'react';
+import {withRouter} from 'react-router';
+import app from './Auth';
 
 function SignUp() {
-    return ()
+    const handleSignUp = useCallback( async e => {
+        e.preventDefault();
+
+        const { email, password } = e.target.elements;
+        try {
+            await app
+                .auth()
+                .createUserWithEmailAndPassword(email.value, password.value);
+                history.push("/");
+        } catch (error){
+            alert(error);
+        }
+    }, [history] );
+
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <form onSubmit={handleSignUp}>
+          <div>
+            <label htmlFor="email">
+              Email
+              <input type="email" name="email" />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="password">
+              Password
+              <input type="password" name="password" />
+            </label>
+          </div>
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    );
 }
+
+export default withRouter(SignUp);
