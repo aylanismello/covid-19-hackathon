59049470f1aac86b0ea27c4fda1f1120d788f8cf
Jordan Ackerman <jork41989@gmail.com>
2020-03-29 15:44:27 -0400

    added auth to user register

--- supplydrop-api/routes/api/users.js
+++ supplydrop-api/routes/api/users.js
@@ -67,8 +67,16 @@ router.post('/register', (req, res) => {
                 pending_items: [],
                 admin: false
               })
-              newUser.save()
-                .then(user => res.json(user))
+              bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                  if (err) throw err;
+                  newUser.password = hash;
+                  newUser.save()
+                    .then(user => res.json(user))
+                  // .catch(err => console.log(err));
+                })
+              })
+              
        }
        })
 
