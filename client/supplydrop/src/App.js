15da6e192a60e22c896049a1ada9c05065dc9ee3
Aylan Mello <aylanismello@gmail.com>
2020-03-29 02:35:59 -0700

    2 hot

--- client/supplydrop/src/App.js
+++ client/supplydrop/src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import Home from './components/home/Home';
 import Login from './components/auth/Login'
 import SignUp from './components/auth/SignUp';
+import Map from './components/map/Map';
 import { AuthProvider } from './components/auth/Auth';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import PrivateRoute from './components/routes/PrivateRoute';
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const App = () => {
   return (
-    <AuthProvider>
+    // <AuthProvider>
       <Router>
         <div>
-          <PrivateRoute exact path="/" component={Home}/>
+          <Route exact path="/" component={Map}/>
+          {/* <PrivateRoute exact path="/" component={Home}/>
           <Route exact path='/login' component={Login}/>
-          <Route exact path='/signup' component={SignUp}/>
+          <Route exact path='/signup' component={SignUp}/> */}
         </div>
       </Router>
-    </AuthProvider>
+    // </AuthProvider>
   )
 }
 export default App;
